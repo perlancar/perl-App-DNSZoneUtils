9f800df27631c47930fb5b6a2bca6c39825aef7f
perlancar (on pc-office) <perlancar@gmail.com>
2019-08-19 21:00:04 +0700

    Initial commit

---
+++ lib/App/DNSZoneUtils.pm
@@ -0,0 +1,19 @@
+package App::DNSZoneUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of CLI utilities related to DNS zones
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+DNS zones:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
